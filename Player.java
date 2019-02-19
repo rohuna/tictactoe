53c99e68b2ea7a36d1db29edb8bad6e5d56f0b83
rohuna <agrawal.rohun@gmail.com>
2019-02-18 16:24:37 -0800

    Update Player.java
    Added some comments for the time being. Later will create a new Player.java file that will act as a template to be used during the meeting. I will only delete the code that is crucial to creating the argument itself (no variable declarations or simple if statements).

--- Player.java
+++ Player.java
@@ -27,11 +27,12 @@ class Player {
      * A score of -100 = human player wins, and a score of +100 = AI wins
      */
     private int minimax(boolean maximizing, int depth) {
+        //return 0 if tie
         if (board.isTie()) return 0;
         // prioritize lesser depth moves
         if (board.checkWin(this.computerSymbol)) return 100 - depth;
         if (board.checkWin(this.humanSymbol)) return -100 + depth;
-
+        
         int best = 0;
         // we want O to win => choose the highest score
         if (maximizing) best = -100;
@@ -40,31 +41,50 @@ class Player {
 
         ArrayList<Move> possibleMove = board.getPossibleMoves();
         ArrayList<Move> bestMoves = new ArrayList<>();
+        
+        //for every possible move, adjust best and bestMoves accordingly
         for (Move move : possibleMove) {
+            
+            //play the move
             board.playMove(move);
+            
+            //recursively call minimax where mximizing is switched and depth increasesd
             int moveScore = minimax(!maximizing, depth + 1);
-
+            
+            //determine best based on moveScore depending on maximizing or minimizing
             if (maximizing) {
+                
+                /*if the best is less than the current move score, set best as the
+                current move, and reset best moves with the current move*/
                 if (best < moveScore) {
                     bestMoves.clear();
                     best = moveScore;
                     bestMoves.add(move);
-                } else if (best == moveScore) {
+                } 
+                //if best is the same as the current move score, add it to bestMoves
+                else if (best == moveScore) {
                     bestMoves.add(move);
                 }
             } else {
+                
+                /*if the best is greater than the current move score, set best as the
+                current move, and reset best moves with the current move*/
                 if (best > moveScore) {
                     bestMoves.clear();
                     best = moveScore;
                     bestMoves.add(move);
-                } else if (best == moveScore) {
+                } 
+                //if best is the same as the current move score, add it to bestMoves
+                else if (best == moveScore) {
                     bestMoves.add(move);
                 }
             }
-
+            
+            //undo the move to avoid changing the board
             board.undoMove(move);
         }
-
+        
+        //base case: depth is 0 and the best move is achieved
         if (depth == 0) {
             // Set this.bestMove with the most optimal move.
             this.bestMove = bestMoves.get(new Random().nextInt(bestMoves.size()));
